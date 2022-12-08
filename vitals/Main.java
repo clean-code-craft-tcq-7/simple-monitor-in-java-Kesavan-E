24f5197f2a1e418c0b8f07960abff7d3f1578f61
Kesavan <116025852+Kesavan-E@users.noreply.github.com>
2022-12-08 21:45:56 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,23 +1,33 @@
 package vitals;
 
-public class Main {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
-    }
+import vitals.BatteryComponents.types.LiionBattery;
+import vitals.BatteryComponents.utils.BatteryHealthCheckUtility;
 
+public class Main {
+   
     public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
+    	
+    	
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(25, 70, 0.7f)) == true);
+
+    	
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(-1, 70, 0.7f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(0, 70, 0.7f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(45, 70, 0.7f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(105, 70, 0.7f)) == false);
+        
+        
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, -5, 0.0f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, 2, 0.0f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, 80, 0.0f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, 85, 0.0f)) == false);
+        
+        
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, 70, -0.0f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, 70, 0.0f)) == false);
+        assert(BatteryHealthCheckUtility.batteryIsOk(new LiionBattery(30, 70, 0.8f)) == false);
+
+        
+        System.out.println("tested ok");
     }
 }
